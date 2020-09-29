b3a316d842d3c56cee114f3b77e818233d6a6737
Usman-Naeem <usman.naeem2212@gmail.com>
2020-09-29 19:47:05 +0500

    second refector

--- index.py
+++ index.py
@@ -15,9 +15,13 @@ req_url = "https://www.muhammadumerfarooq.me"
 
 if conn.is_image(req_url) :
     xml_1 = conn.get_xml(req_url, "url")
+    print(conn.validate(xml_1, "url"))
     conn.write_on_file(xml_1, "sitemap_1.xml")
+
     xml_2 = conn.get_xml(req_url, "image")
+    print(conn.validate(xml_2, "image"))
     conn.write_on_file(xml_2, "sitemap_2.xml")
 else :
     xml = conn.get_xml(req_url, "url")
+    print(conn.validate(xml, "url"))
     conn.write_on_file(xml, "sitemap.xml")
--- src/sitemap.py
+++ src/sitemap.py
@@ -8,6 +8,7 @@ __email__ = "usman.naeem2212@gmail.com"
 __status__ = "Production"
 
 from src.parser import Parser
+import re
 p = Parser()
 con = p.conn()
 
@@ -41,19 +42,17 @@ class SiteMap:
         html = con.get_html(url)
         if what == "url" :
             urls = con.get_links(html)
-            xml = '''<?xml version='1.0' encoding='UTF-8'?>\n<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>
-            '''
+            xml = "<?xml version='1.0' encoding='UTF-8'?><urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>"
             for i in range(len(urls)) :
-                xml += "\n  <url>\n    <loc>" + url + str(urls[i]) + "</loc>\n    <lastmod>" + con.today_date() + "</lastmod>\n  </url>"
-            xml += "\n</urlset>"
+                xml += "<url><loc>" + url + str(urls[i]) + "</loc><lastmod>" + con.today_date() + "</lastmod></url>"
+            xml += "</urlset>"
             return xml
         elif what == "image" :
             urls = con.get_image_links(html)
-            xml = '''<?xml version='1.0' encoding='UTF-8'?>\n<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>\n  <url>
-            '''
+            xml = "<?xml version='1.0' encoding='UTF-8'?><urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'><url>"
             for i in range(len(urls)) :
-                xml += "\n    <image:image>\n      <image:loc>" + url + str(urls[i]) + "</image:loc>\n    </image:image>"
-            xml += "\n  </url>\n<urlset>"
+                xml += "<image:image><image:loc>" + url + str(urls[i]) + "</image:loc></image:image>"
+            xml += "</url></urlset>"
             return xml
 
     @staticmethod
@@ -87,3 +86,62 @@ class SiteMap:
         '''
         with open(name, 'w') as file:
             file.write(xml)
+
+    @staticmethod
+    def validate(x, type) :
+        '''
+        validates xml
+        Args:
+            xml: xml
+        Returns:
+            None
+        Raises:
+            None.
+        '''
+        xml = str(x)
+        if type == "url" :
+            if re.search("^<.*xml version='.+' encoding='UTF-8'", xml) :
+                if re.search(".+<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/...'>", xml) :
+                    w = xml.split("<urlset")
+                    if not w[1].endswith("</urlset>") : return "Error in </urlset> end tag"
+                    w = w[1]
+                    x = w[:(len(w)-9)].split("<url>")
+                    y = []
+                    for value in x :
+                        if value == x[0] : continue
+                        if value.endswith("</url>"):
+                            y.append(value[:(len(value)-6)])
+                        else : return "missing </url> end tag"
+                    for value in y :
+                        x = value.split("</loc>")
+                        if not x[0].startswith("<loc>") : return "Error in </loc> end tag"
+                        x = value.split("<lastmod>")
+                        if not x[1].endswith("</lastmod>") : return "Error in </lastmod> end tag"
+                else : return "Error in <urlset...> tag"
+            else : return "Error in <?xml...> tag"
+            return "Validated xml Successfully. No Errors"
+        elif type == "image" :
+            if re.search("^<.*xml version='.+' encoding='UTF-8'", xml) :
+                if re.search(".+<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/...'>", xml) :
+                    w = xml.split("<urlset")
+                    if w[1].endswith("</urlset>") :
+                        w = w[1]
+                    else : return "Error in </urlset> end tag"
+                    x = w[:(len(w)-9)].split("<url>")
+                    if x[1].endswith("</url>") :
+                        y = x[1][:(len(x[1])-6)].split("<image:image>")
+                    else : return "Error in </url> end tag"
+                    z = []
+                    for value in y :
+                        if value == y[0] : continue
+                        if value.endswith("</image:image>") :
+                            z.append(value[:(len(value)-14)])
+                        else : return "Error in </image:image> end tag"
+                    for value in z :
+                        x = value.split("<image:loc>")
+                        for a in x :
+                            if a == x[0] : continue
+                            if not a.endswith("</image:loc>") : return "Error in </image:loc> end tag"
+                else : return "Error in <urlset...> tag"
+            else : return "Error in <?xml...> tag"
+            return "Validated xml Successfully. No Errors"
